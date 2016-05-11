81ca92299011e3676b19473e2072c1be2250658c
Sky <wufazhuceme@163.com>
2016-05-11 18:33:56 +0800

    添加了动画效果以及图片样式

--- src/com/example/game2048/Card.java
+++ src/com/example/game2048/Card.java
@@ -2,23 +2,31 @@ package com.example.game2048;
 
 import android.content.Context;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.FrameLayout.LayoutParams;
 
 public class Card extends FrameLayout {
     private int num = 0;
     private TextView label;//呈现文字用
-    private TextView test;
+    
     public Card(Context context) {
         super(context);
-        label = new TextView(getContext());
+		LayoutParams lp = null;
+		
+		lp = new LayoutParams(-1, -1);//-1代表填充满整个父级容器
+		lp.setMargins(10, 10, 0, 0);//设置间隔，左边和上边都是10，如果设置为0，这里就会看不到边界，每个card都是连成一片，这个间隔是card的内间隔吧
+		
+		background = new View(getContext());//这里和上次不同了，多了一个background的View
+		background.setBackgroundColor(0x33ffffff);//这层背景起到了蒙版的效果，调节alpha的值，实际上看到的是底部的颜色
+		addView(background, lp);
+        label = new TextView(getContext());//新建了一个TextView
         label.setTextSize(32);
-        label.setTextColor(0xffffffff);
-        label.setBackgroundColor(0xff464848);//设置文字所处区域的背景色ff2C3135
+//        label.setTextColor(0xffffffff);
+//        label.setBackgroundColor(0xff464848);//设置文字所处区域的背景色ff2C3135
         label.setGravity(Gravity.CENTER);//将内容设置居中
         
-        LayoutParams lp = new LayoutParams(-1, -1);//-1代表填充满整个父级容器
-        lp.setMargins(10, 10, 0, 0);//设置间隔，左边和上边都是10
         addView(label, lp);
         
         setNum(0);
@@ -35,11 +43,103 @@ public class Card extends FrameLayout {
 		if(num <= 0){
 			label.setText("");
 		}else{
-			label.setText(num+"");//如果这里直接传入num，它会把这个整形值当做某个资源的id，不能直接传入整形num
+			switch(num){
+			case 2:
+				label.setText("工兵");
+				break;
+			case 4:
+				label.setText("排长");
+				break;
+			case 8:
+				label.setText("连长");
+				break;
+			case 16:
+				label.setText("营长");
+				break;
+			case 32:
+				label.setText("团长");
+				break;
+			case 64:
+				label.setText("旅长");
+				break;
+			case 128:
+				label.setText("师长");
+				break;
+			case 256:
+				label.setText("军长");
+				break;
+			case 512:
+				label.setText("司令");
+				break;
+			case 1024:
+				label.setText("元帅");
+				break;
+			case 2048:
+				label.setText("主席");
+				break;
+			default:
+				label.setText("传说");
+				break;
+			}
+//			label.setText(num+"");//如果这里直接传入num，它会把这个整形值当做某个资源的id，不能直接传入整形num
+		}
+
+		switch (num) {
+		case 0:
+			label.setBackgroundColor(0x00000000);
+			break;
+		case 2:
+			label.setBackgroundColor(0xffeee4da);
+			break;
+		case 4:
+			label.setBackgroundColor(0xffede0c8);
+			break;
+		case 8:
+			label.setBackgroundColor(0xfff2b179);
+			break;
+		case 16:
+			label.setBackgroundColor(0xfff59563);
+			break;
+		case 32:
+			label.setBackgroundColor(0xfff67c5f);
+			break;
+		case 64:
+			label.setBackgroundColor(0xfff65e3b);
+			break;
+		case 128:
+			label.setBackgroundColor(0xffedcf72);
+			break;
+		case 256:
+			label.setBackgroundColor(0xffedcc61);
+			break;
+		case 512:
+			label.setBackgroundColor(0xffedc850);
+			break;
+		case 1024:
+			label.setBackgroundColor(0xffedc53f);
+			break;
+		case 2048:
+			label.setBackgroundColor(0xffedc22e);
+			break;
+		default:
+			label.setBackgroundColor(0xff3c3a32);
+			break;
 		}
 	}
+
+	public boolean equals(Card o) {
+		return getNum()==o.getNum();
+	}
 	
-	public boolean equals(Card o){
-		return getNum() == o.getNum();
-	}//只需要判断两张卡片的数字是否相同就可以了
+	protected Card clone(){//这个clone应该是把当前的card复制一个返回去
+		Card c= new Card(getContext());
+		c.setNum(getNum());
+		return c;
+	}
+
+	public TextView getLabel() {
+		return label;//label就是TextView
+	}
+	
+	private View background;
 }
--- src/com/example/game2048/GameView.java
+++ src/com/example/game2048/GameView.java
@@ -8,308 +8,330 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Point;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.GridLayout;
+import android.widget.LinearLayout;
 
-public class GameView extends GridLayout {
-//让这个类绑定xml，拷贝它的全路径com.example.game2048.GameView
-	public GameView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
-		Log.i("mDebug", "GameView context, attr, defStyle"+attrs.getAttributeValue(0));
-		initGameView();
-		
-	}
-
+public class GameView extends LinearLayout {//这里的GameView是继承自LinearLayout了
+	
 	public GameView(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
-		Log.i("mDebug", "GameView context");
+
 		initGameView();
-		
 	}
 
 	public GameView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		Log.i("mDebug", "GameView context, attr"+attrs.getAttributeValue(0));
-		// TODO Auto-generated constructor stub
+
 		initGameView();
 	}
-	
+
 	private void initGameView(){
-		setColumnCount(4);//设置GridLayout为4列 
-		setBackgroundColor(0xff2C3135);//设置背景色
-		
+		setOrientation(LinearLayout.VERTICAL);
+//		setBackgroundColor(0xffbbada0);//大背景色
+		setBackgroundColor(0xffD3D2CC);
+
 		setOnTouchListener(new View.OnTouchListener() {
-			
-			private float startX, startY, offsetX, offsetY;
-			
+
+			private float startX,startY,offsetX,offsetY;
+
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
-				// TODO Auto-generated method stub
-				switch(event.getAction()){
-					case MotionEvent.ACTION_DOWN:
-						startX = event.getX();
-						startY = event.getY();
-						break;
-					case MotionEvent.ACTION_UP:
-						offsetX = event.getX() - startX;
-						offsetY = event.getY() - startY;
-						
-						if(Math.abs(offsetX) > Math.abs(offsetY)){
-							//区分出水平距离大还是 垂直距离大
-							if(offsetX < -5){//代表向左移动
-								swipeLeft();
-							}else if(offsetX > 5){//代表向右移动
-								swipeRight();
-							}
-						}else{
-							if(offsetY < -5){//代表向上移动
-								swipeUp();
-							}else if(offsetY > 5){//代表向下移动
-								swipeDown();
-							}
-						}						
-						
-						break;
-					
+
+				switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					startX = event.getX();
+					startY = event.getY();
+					break;
+				case MotionEvent.ACTION_UP:
+					offsetX = event.getX()-startX;
+					offsetY = event.getY()-startY;
+
+
+					if (Math.abs(offsetX)>Math.abs(offsetY)) {
+						if (offsetX<-5) {
+							swipeLeft();
+						}else if (offsetX>5) {
+							swipeRight();
+						}
+					}else{
+						if (offsetY<-5) {
+							swipeUp();
+						}else if (offsetY>5) {
+							swipeDown();
+						}
+					}
+
+					break;
 				}
 				return true;
-				//因为这整个事件是连续的，ACTION_MOVE要在ACTION_DOWN之后才行，如果这里返回false，那么move就不会进行了
 			}
-		});		
+		});
 	}
-	
-	//动态计算手机屏幕的宽高
+
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-		// TODO Auto-generated method stub
 		super.onSizeChanged(w, h, oldw, oldh);
-		Log.i("mDebug", "onSizeChanged");
-		
-		int cardWidth = (Math.min(w, h)-10)/4;//减10是为了给右边留下10像素的空隙
 
-		
-		addCards(cardWidth, cardWidth);
-		//手机直立和平放的时候，布局会发生变化，为了不让它变化，需要配置一下AndroidManifest文件
-		
+		Config.CARD_WIDTH = (Math.min(w, h)-10)/Config.LINES;
+
+		addCards(Config.CARD_WIDTH,Config.CARD_WIDTH);
+
 		startGame();
 	}
-	
-	private void addCards(int cardWidth, int cardHeight){
+
+	private void addCards(int cardWidth,int cardHeight){
+
 		Card c;
+
+		LinearLayout line;//这里并没有指定line是水平排列还是垂直排列
+		LinearLayout.LayoutParams lineLp;
 		
-		for(int y = 0; y < 4; y++){//一行一行添加
-			for(int x = 0; x < 4; x++){
+		for (int y = 0; y < Config.LINES; y++) {
+			line = new LinearLayout(getContext());
+			lineLp = new LinearLayout.LayoutParams(-1, cardHeight);//-1是充满宽度的意思，第二个参数是高度值ֵ
+			addView(line, lineLp);
+			
+			for (int x = 0; x < Config.LINES; x++) {
 				c = new Card(getContext());
-				c.setNum(0);
-				addView(c, cardWidth, cardHeight);
-				
-				cardsMap[x][y] = c;//用二位数组来记录这个方阵
+				line.addView(c, cardWidth, cardHeight);
+
+				cardsMap[x][y] = c;
 			}
 		}
 	}
-	
-	private void startGame(){
-		MainActivity.getMainActivity().clearScore();//这里是把主活动的数据清除了
-		
-		for(int y = 0; y < 4; y++){
-			for(int x = 0; x < 4; x++){
+
+	public void startGame(){
+
+		MainActivity aty = MainActivity.getMainActivity();
+		aty.clearScore();
+		aty.showBestScore(aty.getBestScore());
+
+		for (int y = 0; y < Config.LINES; y++) {
+			for (int x = 0; x < Config.LINES; x++) {
 				cardsMap[x][y].setNum(0);
 			}
 		}
 
-		addRandowNum();
-		addRandowNum();
-		
+		addRandomNum();
+		addRandomNum();
 	}
-	
-	private void addRandowNum(){
-		
-		emptyPoints.clear();//这里的清除无关于cardsMap，所以不会影响到游戏开始时的随机数字
-		//遍历所有卡片
-		for(int y = 0; y < 4; y++){
-			for(int x = 0; x < 4; x++){
-				if(cardsMap[x][y].getNum() <= 0){//只有方格内为空才能添加值
-					emptyPoints.add(new Point(x, y));//这个Point里的x和y应该只是一种标记，对应到以后使用
-				}
-			}
-		}
-		
-		Point p = emptyPoints.remove((int)(Math.random()*emptyPoints.size()));//随机的移除某个点，移除了有什么用呢，移除了才能给这个点设置数字么
-		
-		cardsMap[p.x][p.y].setNum(Math.random() > 0.1? 2 : 4);//这样就添加一个随机的数值到p所处的位置，2的概率远远大于4
-	}
-	
-	private void swipeLeft(){
-		
-		boolean merge = false;//
-		for(int y = 0; y < 4; y++){
-			for(int x = 0; x < 4; x++){
-				for(int x1 = x+1; x1 < 4; x1++){//遍历当前位置右边的值，这里的循环是x与右边的三个值得一一比较
-					if(cardsMap[x1][y].getNum() > 0){//如果大于0，则说明不为空
-						if(cardsMap[x][y].getNum() <= 0){//如果当前位置是空的
-							cardsMap[x][y].setNum(cardsMap[x1][y].getNum());//就把右边有效的值放进来，并且清掉右边的值
-							cardsMap[x1][y].setNum(0);
-							
-							x--;//减减是为了让一个位置的右边值都充分比较
-							//这个只有当x的位置为空的时候才会减减一次，
-							merge = true;
-						}else if(cardsMap[x][y].equals(cardsMap[x1][y])){//如果两个值相同，就可以合并了
-							cardsMap[x][y].setNum(cardsMap[x][y].getNum()*2);
-							cardsMap[x1][y].setNum(0);
-							
-							MainActivity.getMainActivity().addScore(cardsMap[x][y].getNum());
-							
-							merge = true;
-						}
-						break;
-					}
-				}
-			}
-		}
-		
-		if(merge == true){
-			addRandowNum();
-			checkComplete();
-		}
-	}
-	
-	private void swipeRight(){
-		boolean merge = false;
-		
-		for(int y = 0; y < 4; y++){
-			for(int x = 3; x >= 0; x--){
-				for(int x1 = x-1; x1 >= 0; x1--){//遍历当前位置右边的值
-					if(cardsMap[x1][y].getNum() > 0){//如果大于0，则说明不为空
-						if(cardsMap[x][y].getNum() <= 0){//如果当前位置是空的
-							cardsMap[x][y].setNum(cardsMap[x1][y].getNum());//就把右边有效的值放进来，并且清掉右边的值
-							cardsMap[x1][y].setNum(0);
-							
-							x++;//在这种情况下多遍历一次，确保如果右边有两个相同的值，就可以将其合并，这里要对应到之后的自减，为了回到原位，所以自增一次
-							
-							merge = true;
-						}else if(cardsMap[x][y].equals(cardsMap[x1][y])){//如果两个值相同，就可以合并了
-							cardsMap[x][y].setNum(cardsMap[x][y].getNum()*2);
-							cardsMap[x1][y].setNum(0);
-							
-							MainActivity.getMainActivity().addScore(cardsMap[x][y].getNum());
-							
-							merge = true;
-						}
-						break;
-					}
-				}
-			}
-		}
-		
-		if(merge == true){
-			addRandowNum();
-			checkComplete();
-		}
-	}
-	
-	private void swipeUp(){
-		boolean merge = false;
-		
-		for(int x = 0; x < 4; x++){
-			for(int y = 0; y < 4; y++){
-				for(int y1 = y+1; y1 < 4; y1++){//遍历当前位置下边的值
-					if(cardsMap[x][y1].getNum() > 0){//如果大于0，则说明下侧不为空
-						if(cardsMap[x][y].getNum() <= 0){//如果当前位置是空的
-							cardsMap[x][y].setNum(cardsMap[x][y1].getNum());//就把下边有效的值放进来，并且清掉下边的值
-							cardsMap[x][y1].setNum(0);
-							
-							y--;//如果右边有两个相同的值，多遍历一次，就可以将其合并
-							
-							merge = true;
-						}else if(cardsMap[x][y].equals(cardsMap[x][y1])){//如果两个值相同，就可以合并了
-							cardsMap[x][y].setNum(cardsMap[x][y].getNum()*2);
-							cardsMap[x][y1].setNum(0);
-							
-							MainActivity.getMainActivity().addScore(cardsMap[x][y].getNum());
-							
-							merge = true;
-						}
-						break;
-					}
-				}
-			}
-		}
-		
-		if(merge == true){
-			addRandowNum();
-			checkComplete();
-		}
-	}
-	
-	private void swipeDown(){
-		boolean merge = false;
-		
-		for(int x = 0; x < 4; x++){
-			for(int y = 3; y >= 0; y--){
-				for(int y1 = y-1; y1 >= 0; y1--){//遍历当前位置下边的值
-					if(cardsMap[x][y1].getNum() > 0){//如果大于0，则说明下侧不为空
-						if(cardsMap[x][y].getNum() <= 0){//如果当前位置是空的
-							cardsMap[x][y].setNum(cardsMap[x][y1].getNum());//就把下边有效的值放进来，并且清掉下边的值
-							cardsMap[x][y1].setNum(0);
-							
-							y++;//如果右边有两个相同的值，多遍历一次，就可以将其合并
-							
-							merge = true;
-						}else if(cardsMap[x][y].equals(cardsMap[x][y1])){//如果两个值相同，就可以合并了
-							cardsMap[x][y].setNum(cardsMap[x][y].getNum()*2);
-							cardsMap[x][y1].setNum(0);
-							
-							MainActivity.getMainActivity().addScore(cardsMap[x][y].getNum());
-							
-							merge = true;
-						}
-						break;
-					}
-				}
-			}
-		}
-		
-		if(merge == true){
-			addRandowNum();
-			checkComplete();
-		}
-	}
-	
-	private void checkComplete(){
-		
-		boolean complete = true;
-		
-		ALL:
-		for(int y = 0; y < 4; y++){
-			for(int x = 0; x < 4; x++){
-				if((cardsMap[x][y].getNum() == 0) || //说明有空位，游戏没结束
-						(x > 0 && cardsMap[x][y].equals(cardsMap[x-1][y])) ||//当与左边比较相等
-						(x < 3 && cardsMap[x][y].equals(cardsMap[x+1][y])) ||//当与右边比较相等
-						(y > 0 && cardsMap[x][y].equals(cardsMap[x][y-1])) ||//当与下边比较相等
-						(y < 3 && cardsMap[x][y].equals(cardsMap[x][y+1]))){//当与上边比较相等，我晕，就是这里越界了，这里写错了，写成+3了
 
-					complete = false;
-					break ALL;//break只能跳出一层循环，所以加all
+	private void addRandomNum(){
+
+		emptyPoints.clear();
+
+		for (int y = 0; y < Config.LINES; y++) {
+			for (int x = 0; x < Config.LINES; x++) {
+				if (cardsMap[x][y].getNum()<=0) {
+					emptyPoints.add(new Point(x, y));
 				}
 			}
 		}
-		
-		if(complete){
-			new AlertDialog.Builder(getContext()).setTitle("你好").setMessage("游戏结束").setPositiveButton("重来", new DialogInterface.OnClickListener() {
+
+		if (emptyPoints.size()>0) {
+
+			Point p = emptyPoints.remove((int)(Math.random()*emptyPoints.size()));//romove掉这个是因为赋值了之后就不为空了，同时也能返回位置
+			cardsMap[p.x][p.y].setNum(Math.random()>0.1?2:4);
+
+			MainActivity.getMainActivity().getAnimLayer().createScaleTo1(cardsMap[p.x][p.y]);//这里就是进行动画效果的地方了，这里是随机生成数字的动画效果
+		}
+	}
+
+
+	private void swipeLeft(){
+
+		boolean merge = false;
+
+		for (int y = 0; y < Config.LINES; y++) {
+			for (int x = 0; x < Config.LINES; x++) {
+
+				for (int x1 = x+1; x1 < Config.LINES; x1++) {
+					if (cardsMap[x1][y].getNum()>0) {
+
+						if (cardsMap[x][y].getNum()<=0) {
+
+							MainActivity.getMainActivity().getAnimLayer().createMoveAnim(cardsMap[x1][y],cardsMap[x][y], x1, x, y, y);
+
+							cardsMap[x][y].setNum(cardsMap[x1][y].getNum());
+							cardsMap[x1][y].setNum(0);
+
+							x--;
+							merge = true;
+
+						}else if (cardsMap[x][y].equals(cardsMap[x1][y])) {
+							MainActivity.getMainActivity().getAnimLayer().createMoveAnim(cardsMap[x1][y], cardsMap[x][y],x1, x, y, y);
+							cardsMap[x][y].setNum(cardsMap[x][y].getNum()*2);
+							cardsMap[x1][y].setNum(0);
+
+							MainActivity.getMainActivity().addScore(cardsMap[x][y].getNum());
+							merge = true;
+						}
+
+						break;
+					}
+				}
+			}
+		}
+
+		if (merge) {
+			addRandomNum();
+			addRandomNum();
+			checkComplete();
+		}
+	}
+	private void swipeRight(){
+
+		boolean merge = false;
+
+		for (int y = 0; y < Config.LINES; y++) {
+			for (int x = Config.LINES-1; x >=0; x--) {
+
+				for (int x1 = x-1; x1 >=0; x1--) {
+					if (cardsMap[x1][y].getNum()>0) {
+
+						if (cardsMap[x][y].getNum()<=0) {
+							MainActivity.getMainActivity().getAnimLayer().createMoveAnim(cardsMap[x1][y], cardsMap[x][y],x1, x, y, y);
+							cardsMap[x][y].setNum(cardsMap[x1][y].getNum());
+							cardsMap[x1][y].setNum(0);
+
+							x++;
+							merge = true;
+						}else if (cardsMap[x][y].equals(cardsMap[x1][y])) {
+							MainActivity.getMainActivity().getAnimLayer().createMoveAnim(cardsMap[x1][y], cardsMap[x][y],x1, x, y, y);
+							cardsMap[x][y].setNum(cardsMap[x][y].getNum()*2);
+							cardsMap[x1][y].setNum(0);
+							MainActivity.getMainActivity().addScore(cardsMap[x][y].getNum());
+							merge = true;
+						}
+
+						break;
+					}
+				}
+			}
+		}
+
+		if (merge) {
+			addRandomNum();
+			addRandomNum();
+			checkComplete();
+		}
+	}
+	private void swipeUp(){
+
+		boolean merge = false;
+
+		for (int x = 0; x < Config.LINES; x++) {
+			for (int y = 0; y < Config.LINES; y++) {
+
+				for (int y1 = y+1; y1 < Config.LINES; y1++) {
+					if (cardsMap[x][y1].getNum()>0) {
+
+						if (cardsMap[x][y].getNum()<=0) {
+							MainActivity.getMainActivity().getAnimLayer().createMoveAnim(cardsMap[x][y1],cardsMap[x][y], x, x, y1, y);
+							cardsMap[x][y].setNum(cardsMap[x][y1].getNum());
+							cardsMap[x][y1].setNum(0);
+
+							y--;
+
+							merge = true;
+						}else if (cardsMap[x][y].equals(cardsMap[x][y1])) {
+							MainActivity.getMainActivity().getAnimLayer().createMoveAnim(cardsMap[x][y1],cardsMap[x][y], x, x, y1, y);
+							cardsMap[x][y].setNum(cardsMap[x][y].getNum()*2);
+							cardsMap[x][y1].setNum(0);
+							MainActivity.getMainActivity().addScore(cardsMap[x][y].getNum());
+							merge = true;
+						}
+
+						break;
+
+					}
+				}
+			}
+		}
+
+		if (merge) {
+			addRandomNum();
+			addRandomNum();
+			checkComplete();
+		}
+	}
+	private void swipeDown(){
+
+		boolean merge = false;
+
+		for (int x = 0; x < Config.LINES; x++) {
+			for (int y = Config.LINES-1; y >=0; y--) {
+
+				for (int y1 = y-1; y1 >=0; y1--) {
+					if (cardsMap[x][y1].getNum()>0) {
+
+						if (cardsMap[x][y].getNum()<=0) {
+							MainActivity.getMainActivity().getAnimLayer().createMoveAnim(cardsMap[x][y1],cardsMap[x][y], x, x, y1, y);
+							cardsMap[x][y].setNum(cardsMap[x][y1].getNum());
+							cardsMap[x][y1].setNum(0);
+
+							y++;
+							merge = true;
+						}else if (cardsMap[x][y].equals(cardsMap[x][y1])) {
+							MainActivity.getMainActivity().getAnimLayer().createMoveAnim(cardsMap[x][y1],cardsMap[x][y], x, x, y1, y);
+							cardsMap[x][y].setNum(cardsMap[x][y].getNum()*2);
+							cardsMap[x][y1].setNum(0);
+							MainActivity.getMainActivity().addScore(cardsMap[x][y].getNum());
+							merge = true;
+						}
+
+						break;
+					}
+				}
+			}
+		}
+
+		if (merge) {
+			addRandomNum();
+			addRandomNum();
+			checkComplete();
+		}
+	}
+
+	private void checkComplete(){
+
+		boolean complete = true;
+
+		ALL:
+			for (int y = 0; y < Config.LINES; y++) {
+				for (int x = 0; x < Config.LINES; x++) {
+					if (cardsMap[x][y].getNum()==0||
+							(x>0&&cardsMap[x][y].equals(cardsMap[x-1][y]))||
+							(x<Config.LINES-1&&cardsMap[x][y].equals(cardsMap[x+1][y]))||
+							(y>0&&cardsMap[x][y].equals(cardsMap[x][y-1]))||
+							(y<Config.LINES-1&&cardsMap[x][y].equals(cardsMap[x][y+1]))) {
+
+						complete = false;
+						break ALL;
+					}
+				}
+			}
+
+		if (complete) {
+			new AlertDialog.Builder(getContext()).setTitle("你好").setMessage("游戏结束").setPositiveButton("重新开始", new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					startGame();
+				}
+			}).setNegativeButton("再看看", new DialogInterface.OnClickListener() {
 				
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
 					// TODO Auto-generated method stub
-					startGame();
+					
 				}
 			}).show();
 		}
+
 	}
-	
-	private Card[][] cardsMap = new Card[4][4];
+
+	private Card[][] cardsMap = new Card[Config.LINES][Config.LINES];
 	private List<Point> emptyPoints = new ArrayList<Point>();
 }
